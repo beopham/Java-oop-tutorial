b1c9e9211fab9e0c80bb00ba4c6e5c480159ae3f
Pham Van Nam <beopham0911.l@gmail.com>
2025-09-27 12:18:58 +0700

    Date commit

---
+++ src/Chapter1_Exercises_on_Classes/MenuDate.java
@@ -0,0 +1,8 @@
+package Chapter1_Exercises_on_Classes;
+
+public class MenuDate {
+    public static void main(String[] args) {
+        Date ngay1=new Date(20,9,2003);
+        System.out.println(ngay1.toString());
+    }
+}
