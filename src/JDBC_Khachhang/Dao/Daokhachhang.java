124440bd684b58a72667f93632d573621a438308
Pham Van Nam <beopham0911.l@gmail.com>
2025-10-25 23:23:56 +0700

    curd lan 1

---
+++ src/JDBC_Khachhang/Dao/Daokhachhang.java
@@ -0,0 +1,37 @@
+package JDBC_Khachhang.Dao;
+
+import JDBC_Khachhang.Model.KhachHang;
+
+import java.util.ArrayList;
+
+public class Daokhachhang implements GenericDA0<KhachHang>{
+    @Override
+    public int Intert(KhachHang khachHang) {
+        return 0;
+    }
+
+    @Override
+    public int Update(KhachHang khachHang) {
+        return 0;
+    }
+
+    @Override
+    public int Delete(String t) {
+        return 0;
+    }
+
+    @Override
+    public ArrayList<KhachHang> selectAll() {
+        return null;
+    }
+
+    @Override
+    public KhachHang selectByid(String t) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<KhachHang> selectById(String t) {
+        return null;
+    }
+}
---
+++ src/JDBC_Khachhang/Database/TestJDBC.java
@@ -0,0 +1,20 @@
+package JDBC_Khachhang.Database;
+
+import JDBC_Khachhang.Database.JDBCUtil;
+import java.sql.Connection;
+
+public class TestJDBC {
+    public static void main(String[] args) {
+        // Gọi hàm getConnection() trong JDBCUtil
+        Connection c = JDBCUtil.getConnection();
+
+        if (c != null) {
+            System.out.println("✅ Kết nối thành công đến MySQL Database!");
+            JDBCUtil.printInfo(c);
+        } else {
+            System.out.println("❌ Kết nối thất bại!");
+        }
+
+        JDBCUtil.closeConnection(c);
+    }
+}
